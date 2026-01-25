9f72bc8abbc910516f1598d7594920760dc9d631
Bazinga9000 <bazonkidonk@gmail.com>
2026-01-25 18:32:15 -0500

    puzzle page update form

--- puzzles/urls.py
+++ puzzles/urls.py
@@ -6,5 +6,6 @@ app_name = "puzzles"
 urlpatterns = [
     # path("", views.index, name="index"),
     path("hunt/<int:hunt_id>/", views.bigboard, name="bigboard"),
-    path("puzzle/<int:puzzle_id>/", views.puzzlepage, name="puzzlepage")
+    path("puzzle/<int:puzzle_id>/", views.puzzlepage, name="puzzlepage"),
+    path("puzzle/<int:puzzle_id>/update", views.update, name="update")
 ]
--- puzzles/views.py
+++ puzzles/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
-from .models import Hunt, Puzzle
+from .models import Hunt, Puzzle, PuzzleStatus, Answer
 
 def bigboard(request, hunt_id):
     hunt = get_object_or_404(Hunt, pk=hunt_id)
@@ -11,4 +12,32 @@ def bigboard(request, hunt_id):
 def puzzlepage(request, puzzle_id):
     puzzle = get_object_or_404(Puzzle, pk=puzzle_id)
 
-    return render(request, "puzzles/puzzlepage.html", {"puzzle": puzzle})
+    return render(request, "puzzles/puzzlepage.html", {
+        "puzzle": puzzle,
+        "puzzle_statuses": PuzzleStatus
+    })
+
+def update(request, puzzle_id):
+    puzzle = get_object_or_404(Puzzle, pk=puzzle_id)
+    
+    # TODO: proper validation
+
+    # Update puzzle status
+    new_status = request.POST["status"]
+    puzzle.status = new_status
+    puzzle.save()
+
+    # Delete any requested answer
+    for answer in puzzle.answer_set.all():
+        if request.POST.get(f"delete_answer_{answer.id}", False):
+            answer.delete()
+
+    # Add new answer
+    new_answer_text = request.POST["new_answer"]
+    if new_answer_text != "" and len(new_answer_text) < 255:
+        ans = Answer()
+        ans.answer_text = new_answer_text.upper()
+        ans.puzzle = puzzle
+        ans.save() 
+
+    return HttpResponseRedirect(reverse("puzzles:puzzlepage", args=(puzzle.id,)))
