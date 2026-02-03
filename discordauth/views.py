303b85bce33020d03d5c0375a1acb0d533917e2f
periodically-makes-puns <eyang10000@gmail.com>
2026-02-02 21:51:28 -0500

    oops remove hardcoding

--- discordauth/views.py
+++ discordauth/views.py
@@ -2,12 +2,21 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.core.exceptions import PermissionDenied
 from django.contrib.auth import authenticate
 from django.urls import reverse
+from dotenv import load_dotenv
+from urllib.parse import quote
+import os
+
+load_dotenv()
+
+CLIENT_ID = os.getenv("CLIENT_ID")
+REDIRECT_URI = os.getenv("REDIRECT_URI")
+
 from uuid import uuid4
 
 def login(request):
     state = uuid4().hex
     request.session["state"] = state
-    return HttpResponse(f"<a href=https://discord.com/oauth2/authorize?client_id=1466557779702714592&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A8080%2Fauth&scope=identify&state={state}>discord sign in</a>")
+    return HttpResponse(f"<a href=https://discord.com/oauth2/authorize?client_id={CLIENT_ID}&response_type=code&redirect_uri={quote(REDIRECT_URI)}&scope=identify&state={state}>discord sign in</a>")
 
 def auth(request):
     if "code" not in request.GET \
