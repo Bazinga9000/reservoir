d3bea505b5fd7fd49799e65517e38d00e0c6a820
Bazinga9000 <bazonkidonk@gmail.com>
2026-02-05 23:00:46 -0500

    basics of prio system

--- puzzles/models.py
+++ puzzles/models.py
@@ -10,6 +10,22 @@ class PuzzleStatus(models.TextChoices):
     NES = 'EX', "Now Extract Somehow" 
     SOLVED = 'SL', "Solved"
 
+    # Return the starting priority value of a puzzle type
+    @staticmethod
+    def default_priority(status):
+        if status == PuzzleStatus.LOCKED:
+            return 0.2 # This is nonzero to account for locked metas
+        elif status == PuzzleStatus.NOT_STARTED:
+            return 1.3
+        elif status == PuzzleStatus.IN_PROGRESS:
+            return 1
+        elif status == PuzzleStatus.STUCK:
+            return 1.75
+        elif status == PuzzleStatus.NES:
+            return 2
+        elif status == PuzzleStatus.SOLVED:
+            return 0
+
 class Hunt(models.Model):
     name = models.CharField(max_length=255)
 
@@ -50,6 +66,38 @@ class Round(models.Model):
     def puzzles_ordered(self):
         # TODO: human sort the name? We might have something weird like shell game with numbered puzzles that goes over 10?
         return self.puzzle_set.order_by("-is_meta", "name").all()
+    
+    # An extra multiplier on the puzzles of this round based on its meta structure
+    def priority_multiplier(self):
+        num_metas = self.puzzle_set.filter(is_meta=True).count()
+
+        # This round has no metas, no adjustment
+        if num_metas == 0:
+            return 1
+        
+        solved_metas = self.puzzle_set.filter(models.Q(is_meta=True) and ~models.Q(status=PuzzleStatus.SOLVED)).count()
+        unsolved_metas = num_metas - solved_metas
+
+        # This round has metas, but all of them are solved, so we lower the round's priority
+        if unsolved_metas == 0:
+            return 0.25 # Nonzero in case a feeder used elsewhere is prioritized
+        
+        # This round has exactly one unsolved meta and no other metas. Give it massive priority. 
+        if num_metas == unsolved_metas and num_metas == 1:
+            return 10
+        
+        # TODO: if we ever explicitly enable setting puzzles as feeders to metas, account for that
+        num_feeders = self.puzzle_set.filter(is_meta=False).count()
+
+        # This round has only metas. It's probably *extremely* important. Give it brobdingnagian priority.
+        if num_feeders == 0:
+            return 50
+
+        # This round has multiple metas and some non-meta feeders and thus something weird is going on. 
+        # It is not quite as important as a feederless round, but still more important than a 1 meta round
+        # so we give it a merely falstaffian priority 
+        return 25
+
 
 class Puzzle(models.Model):
     name = models.CharField(max_length=255)
@@ -78,6 +126,26 @@ class Puzzle(models.Model):
             self.regenerate_puzzle_sheet()
         
         return f"https://docs.google.com/spreadsheets/d/{self.sheet_id}/edit"
+    
+    def priority(self):
+        # Solved puzzles have 0 priority, no need to do any further math
+        if self.status == PuzzleStatus.SOLVED:
+            return 0
+        
+        priority = PuzzleStatus.default_priority(self.status)
+
+        for hunt_round in self.rounds.all():
+            priority *= hunt_round.priority_multiplier()
+
+        # Metas are highly prioritized above and beyond their round multipliers
+        if self.is_meta:
+            priority *= 10
+
+        # An unsolved puzzle with answers??? How strange. Give it a priority boost.
+        if self.answer_set.count() > 0:
+            priority *= 1.5
+
+        return priority
 
 
 class PuzzleRoundField(models.Model):
