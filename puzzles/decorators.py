a9d99094130ed0f4c996d343ff5e1732ddaad16e
periodically-makes-puns <eyang10000@gmail.com>
2026-08-27 23:25:48 -0400

    fix dumb errors

--- puzzles/decorators.py
+++ puzzles/decorators.py
@@ -1,4 +1,4 @@
-from .models import HuntMember
+from .models import HuntMember, Puzzle
 from django.shortcuts import get_object_or_404
 from django.http import HttpResponseRedirect
 
@@ -17,7 +17,7 @@ def user_can_see_puzzle(view_func):
         if not req.user.is_authenticated:
             return HttpResponseRedirect(reverse("puzzles:login"))
         elif HuntMember.objects.filter(hunt=get_object_or_404(Puzzle, id=puzzle_id).hunt, user=req.user).exists():
-            return view_func(req, hunt_id)
+            return view_func(req, puzzle_id)
         else:
             return HttpResponseRedirect("/")
     return inner
